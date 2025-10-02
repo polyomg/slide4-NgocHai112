669c9de21e9969c2301a21e410c7f30b90aecefd
Hai <ngochai310303@gmail.com>
2025-10-02 17:00:56 +0700

    PS43664 - NguyenNgocHai - Updated

--- Slide4/src/main/java/com/example/slide4/Slide4Application.java
+++ Slide4/src/main/java/com/example/slide4/Slide4Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Slide4Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Slide4Application.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Slide4Application.class, args);
+	}
 
 }
